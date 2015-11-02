eb99529a67265375427858ac6bdfebe20e32c1c4
Gwenael <gwenael_gouerou@yahoo.fr>
2015-11-02 16:27:30 +0200

    Create plotTemp.R

---
+++ plotTemp.R
@@ -0,0 +1,26 @@
+## 2013-2015
+# dec31 <- dmy_hm("31-12-2015 23:00",tz="Europe/Vilnius")  
+# with(sd2013, plot(date, temperature, xlab="", ylim=c(-25,25),
+#                   main = "Temperature à Vilnius \n Septembre à Décembre",  
+#                   col="darkseagreen1", pch=8))
+# par(new=T)
+# with(sd2014, plot(date, temperature, xlab="", ylim=c(-25,25)))
+# par(new=T)
+# with(sd2015, plot(date, temperature, xlab="", ylim=c(-25,25), 
+#                   pch=20, col="red", 
+#                   xlim=c(min(date), dec31)))
+# legend("bottomright", legend= c("2013  ","2014  ","2015  "), 
+#        pch=c(8,1,20), col=c("darkseagreen1",1,2), cex=0.8)
+
+
+## 2014-2015  
+dec31 <- dmy_hm("31-12-2015 23:00",tz="Europe/Vilnius")  
+with(sd2014, plot(date, temperature, xlab="", ylim=c(-25,25),
+                  main = "Temperature à Vilnius \n Septembre à Décembre"))
+par(new=T)
+with(sd2015, plot(date, temperature, xlab="", ylim=c(-25,25), 
+                  pch=20, col="red", 
+                  xlim=c(min(date), dec31)))
+legend("bottomright", legend= c("2014  ","2015  "),
+       pch=c(1,20), col=c(1,2), cex=0.8)
+       
